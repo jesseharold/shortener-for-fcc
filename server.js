31304e156ea582a148257ebc2598205b49efe160
jesseharold <jesseharold@gmail.com>
2017-05-08 21:52:12 -0700

    redirect working

--- server.js
+++ server.js
@@ -33,26 +33,35 @@ db.once('open', function() {
 
     app.get("/new/*?", function(req, res){
         // get a random 4 digit number to serve as the alias
-        let alias = getAlias();
-        if (checkURL(req.params[0])){
-            const newAlias = new Redirect({ name: alias, url: req.params[0]});
-            newAlias.save(function (err, newRedirect) {
-                if (err) return console.error(err);
-                res.send("Redirect created successfully: " + alias);
-            });
+        let alias = createAlias(0);
+        if (alias === false){
+            // we ran out of usable aliases
+            res.send("sorry, the shortener is out of usable aliases.");
         } else {
-            res.send("that is not a valid URL.");
+            if (checkURL(req.params[0])){
+                const newAlias = new Redirect({ name: alias, url: req.params[0]});
+                newAlias.save(function (err, newRedirect) {
+                    if (err) return console.error(err);
+                    res.send("Redirect created successfully: " + alias);
+                });
+            } else {
+                res.send("That is not a valid URL starting with http: " + req.params[0]);
+            }
         }
     });
 
     app.get("/:urlAlias", function(req, res){
-        // make sure url exists in datapase
-        if (dummyData.hasOwnProperty(req.params["urlAlias"])){
-            // perform redirect
-            res.redirect(dummyData[req.params["urlAlias"]]);
-        } else {
-            res.send("url not found: " + req.params["urlAlias"]);
-        }
+        // make sure url exists in database
+        Redirect.find({ name:  req.params["urlAlias"]}, function(err, result){
+            if (err){
+                console.log(err);
+            }
+            if(result.length === 0){
+                res.send("redirect not found: " + req.params["urlAlias"]);
+            } else {
+                res.redirect(result[0].url);
+            }
+        });
     });
 
 
@@ -70,18 +79,18 @@ function randomNum(){
     return Math.floor(Math.random()*10).toString();
 }
 
-function getAlias(tries){
+function createAlias(tries){
     // returns a random 4-digit strings
     const alias = randomNum() + randomNum() + randomNum() + randomNum();
     if (checkAlias(alias)){
         return alias;
     } else {
         if (tries > 9990){
-            // stop recursion if all possible aliases are in use
+            // stop recursion if we've tried unsuccessfully close to 10K times
             return false;
         } else {
             // try again for an alias that's not in use
-            getAlias(tries+1);
+            createAlias(tries+1);
         }
     }
 }
